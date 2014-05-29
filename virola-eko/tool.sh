74b95b83a79a4a6fe9fd1337ae2e390d7542edc3
Virola <virola.zhu@gmail.com>
2014-05-29 17:29:00 +0800

    update some files
    …

---
+++ virola-eko/tool.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+
+print "$cmd"
+cmd=$1
+target=$2
+
+if ["$cmd" == "mv"]; then
+    cp -ur ./* "$target"
+fi
+
+if ["$cmd" == "build"]; then
+    edp build -f
+    cp index.html index.bak.html
+    cp output/index.html index.html
+fi 
